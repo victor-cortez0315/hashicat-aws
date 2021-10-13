38cdb9cd92d6599fc2277e9c2ce2d4a1d9d695b9
KiwibankVictor <victor.cortez.jr@gmail.com>
2021-10-13 05:07:39 +0000

    Added the first tag edit

--- main.tf
+++ main.tf
@@ -128,7 +128,7 @@ resource "aws_instance" "hashicat" {
 
   tags = {
     Name = "${var.prefix}-hashicat-instance"
-    aws_instance = "devops"
+    Department = "devops"
     Billable = "true"
   }
 }
